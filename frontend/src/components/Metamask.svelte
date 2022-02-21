23a47969babe36d24ddbb033f709092c2f5453e2
Michael <michael.spengler@sap.com>
2022-02-21 12:23:20 +0100

    add basic frontent features

---
+++ frontend/src/components/Metamask.svelte
@@ -0,0 +1,13 @@
+<script>
+    function enterApp() {
+        alert("drink");
+        if (typeof window.ethereum !== "undefined") {
+            alert("MetaMask is installed!");
+        }
+    }
+</script>
+
+<button on:click={enterApp}> Enter App </button>
+
+<style>
+</style>
