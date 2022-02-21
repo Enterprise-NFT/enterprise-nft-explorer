48b81e26068971942833869762593cd6f3747874
Michael <michael.spengler@sap.com>
2022-02-21 12:28:50 +0100

    add metamask features

--- frontend/src/components/Metamask.svelte
+++ frontend/src/components/Metamask.svelte
@@ -1,13 +1,30 @@
 <script>
+    import { onMount } from "svelte";
+
+    let account;
+
     function enterApp() {
         alert("drink");
         if (typeof window.ethereum !== "undefined") {
-            alert("MetaMask is installed!");
+            console.log("MetaMask is installed!");
+
+            ethereum.request({ method: "eth_requestAccounts" });
         }
     }
+
+    onMount(async () => {
+        const accounts = await ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        account = accounts[0];
+    });
 </script>
 
 <button on:click={enterApp}> Enter App </button>
+
+<p><br /></p>
+
+{account}
 
 <style>
 </style>
