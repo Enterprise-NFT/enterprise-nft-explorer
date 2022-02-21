9697c286122843d8ee53e3ce20b43ee064c835bc
Michael <michael.spengler@sap.com>
2022-02-21 14:57:16 +0100

    add docs for deployment

---
+++ backend/server.ts
@@ -0,0 +1,16 @@
+import { opine, serveStatic } from "https://deno.land/x/opine@2.1.2/mod.ts";
+
+const app = opine();
+const pathToAssets = `${Deno.cwd()}/frontend/docs`
+const pathToIndexHTML = `${pathToAssets}/index.html`
+
+app.use(serveStatic(pathToAssets))
+
+app.get("/", function (req, res) {
+    res.sendFile(pathToIndexHTML);
+});
+
+app.listen(
+    3000,
+    () => console.log("server has started on http://localhost:3000 🚀"),
+);
